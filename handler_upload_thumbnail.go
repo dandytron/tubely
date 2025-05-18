59c50f868bc0a4e181c120ab75de7d58fb6b10cd
dandytron <104713695+dandytron@users.noreply.github.com>
2025-05-18 19:30:09 +0800

    A: Added base64 URL image encoding to see how silly that is

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// Bit shifting is a way to multiply by powers of 2. 10 << 20 is the same as 10 * 1024 * 1024, which is 10MB.
+	const MaxMemory = 10 << 20
+	r.ParseMultipartForm(MaxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	// Get the media type from the form file's Content-Type header
+	mediaType := header.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
+		return
+	}
+
+	// Read all the image data into a byte slice using io.ReadAll
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read image data from file", err)
+		return
+	}
+
+	// Use base64.StdEncoding.EncodeToString from the encoding/base64 package to convert the image data to a base64 string
+	base64DataString := base64.StdEncoding.EncodeToString(imageData)
+
+	// Create a data URL with the media type and base64 encoded image data. The format is: data:<media-type>;base64,<data>
+	thumbnailDataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, base64DataString)
+
+	// Get the video's metadata from the SQLite database. The apiConfig's db has a GetVideo method you can use
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not retrieve video metadata with provided ID", err)
+		return
+	}
+
+	//If the authenticated user is not the video owner, return a http.StatusUnauthorized response
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User ID does not match the video creator's ID", nil)
+		return
+	}
+
+	videoMetadata.ThumbnailURL = &thumbnailDataURL
+
+	// Thumbnail URL should have this format: http://localhost:<port>/api/thumbnails/{videoID}
+	err = cfg.db.UpdateVideo(videoMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
